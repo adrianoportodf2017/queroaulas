5da137d78418a33e81de5f661578dd03fd2910c6
adriano alves <33916451+adrianoportodf2017@users.noreply.github.com>
2022-11-03 11:36:14 -0300

    subindo form checkout

--- application/modules/frontend/views/checkout.php
+++ application/modules/frontend/views/checkout.php
@@ -62,37 +62,40 @@
 
                                                 //  var_dump($this->ion_auth->logged_in());die;
                                                 if (!$this->ion_auth->logged_in()) { ?>
-                                                        <form method="post" id="myform" action="<?php echo site_url('frontend/salvar_cliente/'); ?>" name="myform" onSubmit="return false">
-                                                                <div class="row">
+                                                        <div class="card card-profile">
+                                                <div class="card-body">
+                                                        <div class="card card-plain">
+                                                                <div class="card-body">
+                                                                        <?php if ($message) { ?><div id="infoMessage" class="alert alert-light"><?= $message ?></div><?php } ?>
+                                                                        <form method="post" action="auth/loginClient">
+                                                                                <input type="hidden" class="form-control form-control-lg" aria-label="" name="id" value="<?= $id ?>">
+                                                                                <input type="hidden" class="form-control form-control-lg" aria-label="" name="date" value="<?= $date ?>">
+                                                                                <input type="hidden" class="form-control form-control-lg" aria-label="" name="hour" value="<?= $hour ?>">
 
-                                                                        <div class="col-md-12">
-                                                                                <div class="form-group">
-                                                                                        <input type="text" placeholder="Nome Completo" class="form-control required w3-round" required name="first_name" id="name" />
+                                                                                <div class="mb-3">
+                                                                                        <input type="email" class="form-control form-control-lg" aria-label="Email" name="identity" placeholder="Email" autofocus>
                                                                                 </div>
-                                                                        </div>
-                                                                        <div class="col-md-12">
-                                                                                <div class="form-group">
-
-                                                                                        <input type="email" class="form-control required w3-round email" required id="email" name="email" placeholder="email">
+                                                                                <div class="mb-3">
+                                                                                        <input type="password" class="form-control form-control-lg" name="password" placeholder="Senha">
                                                                                 </div>
-                                                                        </div>
-                                                                        <div class="col-md-12">
-                                                                                <div class="form-group">
-                                                                                        <input class="form-control required min" type="password" required id="password" name="password" placeholder="Senha" min="3">
+                                                                                <div class="form-check form-switch">
+                                                                                        <input class="form-check-input" type="checkbox" id="rememberMe">
+                                                                                        <label class="form-check-label" for="rememberMe">Lembrar-me</label>
                                                                                 </div>
-                                                                        </div>
-                                                                        <div class="col-md-12">
-                                                                                <div class="form-group has-success">
-                                                                                        <input type="text" placeholder="Celular" id="phone" name="phone" class="form-control required w3-round phone" required />
+                                                                                <div class="text-center">
+                                                                                        <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Acessar</button>
                                                                                 </div>
-                                                                        </div>
-                                                                        <div class="form-check">
-                                                                                <label class="" for="customCheck1">Li e concordo com os Termos e Condições e Política de Privacidade</label>
-                                                                                <input class="form-check-input required " require type="checkbox" value="1" id="fcustomCheck1" checked="" name="terms">
-                                                                        </div>
-                                                                        <button class="btn btn-outline-primary btn-block mt-0" id="btn_bt_loader" onclick="validate()">Finalizar</button>
+                                                                        </form>
                                                                 </div>
-                                                        </form>
+                                                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                                                                        <p><a data-toggle="modal" href="#myModal"> Esqueci minha senha</a></p>
+                                                                </div>
+                                                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                                                                        <p><a data-toggle="modal" href="#myCadastro"> Cadastrar</a></p>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        </div>
 
                                                 <?php } else {
                                                         //  var_dump($this->session->userdata->git) 
@@ -213,4 +216,71 @@
         </div>
 </div>
 </div>
+</div>
+
+
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+        <div class="modal-dialog">
+                <div class="modal-content">
+                        <form method="post" action="auth/forgot_password">
+                                <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title">Forgot Password ?</h4>
+                                </div>
+
+                                <div class="modal-body">
+                                        <p>Enter your e-mail address below to reset your password.</p>
+                                        <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+
+                                </div>
+                                <div class="modal-footer">
+                                        <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+                                        <input class="btn detailsbutton" type="submit" name="submit" value="submit">
+                                </div>
+                        </form>
+                </div>
+        </div>
+</div>
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myCadastro" class="modal fade">
+        <div class="modal-dialog">
+                <div class="modal-content">
+                        <div class="card">
+                                <div class="card-body">
+                                        <form method="post" id="myform" action="<?php echo site_url('frontend/salvarClient/'); ?>" name="myform" onSubmit="return false">
+                                                <div class="row">
+                                                        <input type="hidden" class="form-control form-control-lg" aria-label="" name="id" value="<?= $id ?>">
+                                                        <input type="hidden" class="form-control form-control-lg" aria-label="" name="date" value="<?= $date ?>">
+                                                        <input type="hidden" class="form-control form-control-lg" aria-label="" name="hour" value="<?= $hour ?>">
+                                                        <div class="col-md-12">
+                                                                <div class="form-group">
+                                                                        <input type="text" placeholder="Nome Completo" class="form-control required w3-round" required name="first_name" id="name" />
+                                                                </div>
+                                                        </div>
+                                                        <div class="col-md-12">
+                                                                <div class="form-group">
+
+                                                                        <input type="email" class="form-control required w3-round email" required id="email" name="email" placeholder="email">
+                                                                </div>
+                                                        </div>
+                                                        <div class="col-md-12">
+                                                                <div class="form-group">
+                                                                        <input class="form-control required min" type="password" required id="password" name="password" placeholder="Senha" min="3">
+                                                                </div>
+                                                        </div>
+                                                        <div class="col-md-12">
+                                                                <div class="form-group has-success">
+                                                                        <input type="text" placeholder="Celular" id="phone" name="phone" class="form-control required w3-round phone" required />
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-check">
+                                                                <label class="" for="customCheck1">Li e concordo com os Termos e Condições e Política de Privacidade</label>
+                                                                <input class="form-check-input required " require type="checkbox" value="1" id="fcustomCheck1" checked="" name="terms">
+                                                        </div>
+                                                        <button class="btn btn-outline-primary btn-block mt-0" id="btn_bt_loader" onclick="validateClient()">Finalizar</button>
+                                                </div>
+                                        </form>
+                                </div>
+                        </div>
+                </div>
+        </div>
 </div>
