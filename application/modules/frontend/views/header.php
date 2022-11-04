da2f87f1b89440ca088ed103bd0c963cb2d5e8fb
adriano <adrianobr00@gmail.com>
2022-11-04 15:27:25 -0300

    subindo botoes nova cor e utf8

--- application/modules/frontend/views/header.php
+++ application/modules/frontend/views/header.php
@@ -8,11 +8,13 @@ $site_name = $this->db->get('settings')->row()->title;
 setlocale(LC_ALL,'pt_BR.UTF8');
 mb_internal_encoding('UTF8'); 
 mb_regex_encoding('UTF8');
+header('Content-Type: text/html; charset=utf-8');
+
 
 ?>
 
 <head>
-    <meta charset="utf-8" />
+<meta charset="utf-8" /> 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
@@ -202,21 +204,12 @@ mb_regex_encoding('UTF8');
         border: 1px solid black;
     }
 
-    .buttonhours {
-        height: 50px;
-        position: relative;
-        margin: 5px;
-        margin-left: 10px;
-        width: 97%;
-        border-style: 5px solid red;
-        padding: 3px;
-
-    }
-
-    .button-week {
+       .button-week {
         position: relative;
         margin-bottom: 2px;
         margin-left: 10px;
+        background-color: #405cf5;
+
 
         text-align: center;
         font-weight: bold;
@@ -239,6 +232,45 @@ mb_regex_encoding('UTF8');
         background-color: #4c0bce;
         color: white;
     }
+
+/* CSS */
+ .buttonhours{
+  appearance: button;
+  backface-visibility: hidden;
+  background-color: #405cf5;
+  border-radius: 6px;
+  border-width: 0;
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset,rgba(50, 50, 93, .1) 0 2px 5px 0,rgba(0, 0, 0, .07) 0 1px 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  font-family: -apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif;
+  font-size: 100%;
+  height: 44px;
+  line-height: 1.15;
+  margin: 12px 0 0;
+  outline: none;
+  overflow: hidden;
+  padding: 0 25px;
+  position: relative;
+  text-align: center;
+  text-transform: none;
+  transform: translateZ(0);
+  transition: all .2s,box-shadow .08s ease-in;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 95%;
+}
+
+ .buttonhours:disabled {
+  cursor: default;
+}
+
+ .buttonhours:focus {
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .2) 0 6px 15px 0, rgba(0, 0, 0, .1) 0 2px 2px 0, rgba(50, 151, 211, .3) 0 0 0 4px;
+}
+
 </style>
 
 
